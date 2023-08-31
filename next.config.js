8575b946c6d438ee325f106a7b21e723e8916cb3
Jonathan Malewa <139697835+jonathanmalewa@users.noreply.github.com>
2023-08-31 22:17:13 +0800

    Blog Next JS dengan auth dan backend
    masih dalam proses deploy, karna sambil belajar juga, Hehehe

---
+++ next.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    domains: ["images.pexels.com"],
+  },
+}; //untuk menambahkan external domain agar dapat mengexport nya
+
+module.exports = nextConfig;
